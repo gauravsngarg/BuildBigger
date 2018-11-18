0b6fe31b5712cb5b575c0e3489ea06005e0a167c
Gaurav Garg <gauravsngarg@gmail.com>
2018-11-18 15:20:47 +0530

    Add  TextView to display Joke through Android Lib

--- androidlibrary/src/main/java/example/com/androidlibrary/MainLibraryActivity.java
+++ androidlibrary/src/main/java/example/com/androidlibrary/MainLibraryActivity.java
@@ -1,13 +1,21 @@
 package example.com.androidlibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainLibraryActivity extends AppCompatActivity {
+
+    private TextView tv_joke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_library);
+
+        tv_joke = (TextView) findViewById(R.id.tv_joke);
+
+        String joke = getIntent().getExtras().getString("joke");
+        tv_joke.setText(joke);
     }
 }
