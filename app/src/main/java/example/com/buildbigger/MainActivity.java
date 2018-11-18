e072c970961bee39b319f951378eeb17afae0bfc
Gaurav Garg <gauravsngarg@gmail.com>
2018-11-18 15:14:42 +0530

    Display Joke Using Toast

--- app/src/main/java/example/com/buildbigger/MainActivity.java
+++ app/src/main/java/example/com/buildbigger/MainActivity.java
@@ -1,13 +1,33 @@
 package example.com.buildbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.Joker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btShowJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btShowJoke = (Button) findViewById(R.id.btnShowJoke);
+
+        final Joker joker = new Joker();
+        btShowJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, joker.getJoke(),Toast.LENGTH_SHORT).show();
+               // Intent intent = new Intent(MainActivity.this, MainLibraryActivity.class);
+                //intent.putExtra("joke", joker.getJoke());
+                //startActivity(intent);
+            }
+        });
     }
 }
